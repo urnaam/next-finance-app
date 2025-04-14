3e3fc3844e53a2c20b5868faaa11c6a90099fe31
urnaam <d.se11d004@gmail.com>
2025-04-14 20:04:25 +0900

    direct to login

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
+'use client';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+    if (!isLoggedIn) {
+      router.push('/login'); // Redirect to login page if not logged in
+    }
+  }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
